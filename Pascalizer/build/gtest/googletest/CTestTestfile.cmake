e8211c75baf55e3b2b86d57ddcb2b90f3ce71824
1drid1 <ivandor228@outlook.com>
2025-05-24 02:48:22 +0300

    added /Pascalizer/build to .gitignore fixed CMakeLists minor bug fixes in tests

--- Pascalizer/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/4elodoy Molovek/source/repos/Pascalizer/Pascalizer/gtest/googletest
-# Build directory: C:/Users/4elodoy Molovek/source/repos/Pascalizer/Pascalizer/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
